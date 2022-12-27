71fb60bdc87cbab1511f3861ad9d5f8b658164a4
SorenDeveloppement <sorenel@yahoo.com>
2022-12-27 23:08:33 +0100

    The desing seems like a poop v.1

--- src/fr/galaglow/nv/Main.java
+++ src/fr/galaglow/nv/Main.java
@@ -23,6 +23,15 @@ public class Main extends JFrame {
         JPanel contentPane = (JPanel) this.getContentPane();
         contentPane.setLayout(new FlowLayout());
 
+        contentPane.add(ipPanel());
+        contentPane.add(portPanel());
+
+    }
+
+    private JPanel ipPanel() {
+        JPanel ipContent = new JPanel();
+        ipContent.setLayout(new BoxLayout(ipContent, BoxLayout.Y_AXIS));
+
         // Setting up the ip result list
         JList<String> iplist = new JList<>();
         iplist.setListData(new String[]{"Null"});
@@ -31,21 +40,44 @@ public class Main extends JFrame {
         iplist.setVisibleRowCount(-1);
 
         JScrollPane listIpScroller = new JScrollPane(iplist);
-        listIpScroller.setPreferredSize(new Dimension(250, 80));
-        contentPane.add(listIpScroller);
+        listIpScroller.setPreferredSize(new Dimension(300, 200));
+        ipContent.add(listIpScroller);
+
+        // Setting up the selected ip host name
+        JLabel ipHostName = new JLabel("No Ip Selected");
+        ipContent.add(ipHostName);
 
         // Setting up the local ip scan button
         JButton ipScanButton = new JButton("Start an local ip scan");
-        contentPane.add(ipScanButton);
+        ipContent.add(ipScanButton);
+
+        return ipContent;
+    }
+
+    private JPanel portPanel() {
+        JPanel portContent = new JPanel();
+        portContent.setLayout(new BoxLayout(portContent, BoxLayout.Y_AXIS));
+
+        // Setting up the port result list
+        JList<String> portlist = new JList<>();
+        portlist.setListData(new String[]{"Null", "AA", "aa", "eee"});
+        portlist.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        portlist.setLayoutOrientation(JList.VERTICAL);
+        portlist.setVisibleRowCount(-1);
+
+        JScrollPane listPortScroller = new JScrollPane(portlist);
+        listPortScroller.setPreferredSize(new Dimension(150, 200));
+        portContent.add(listPortScroller);
 
         // Seting up the ip text field for port scan
         JTextField ipTextField = new JTextField("192.168.1.1");
-        contentPane.add(ipTextField);
+        portContent.add(ipTextField);
 
         // Setting up the port scan button
         JButton portScanButton = new JButton("Start a port scan");
-        contentPane.add(portScanButton);
+        portContent.add(portScanButton);
 
+        return portContent;
     }
 
     public static void main(String[] args) {
